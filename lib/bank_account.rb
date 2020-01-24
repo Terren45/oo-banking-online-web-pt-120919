d33ed373bf0841f6ee992e5a37a9dd5296a5cdaf
Terren Culberson <culbersonterren@yahoo.com>
2020-01-24 03:33:11 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
-
+  
+   attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(cash)
+  @balance += 1000
+ end
+ 
+  def display_balance
+   "Your balance is $#{@balance}."
+  end
+  
+  def vaild?
+    @status == "open" && if balance > 0 ? true : false  
+  end
+  
+  def close_account
+    @status = account 
+  end
 end
+
